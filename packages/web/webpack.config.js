ef33d79ac680add50f7bb1995110b642b9f69309
Adrian Rocke <me@adrianrocke.com>
2023-02-25 12:06:59 -0600

    build: dev server api proxy

--- packages/web/webpack.config.js
+++ packages/web/webpack.config.js
@@ -1,9 +1,12 @@
 const { composePlugins, withNx } = require('@nrwl/webpack');
 const { withReact } = require('@nrwl/react');
 
-// Nx plugins for webpack.
 module.exports = composePlugins(withNx(), withReact(), (config) => {
-  // Update the webpack config as needed here.
-  // e.g. `config.plugins.push(new MyPlugin())`
+  if (config.devServer) {
+    config.devServer.proxy = {
+      '/api': 'http://localhost:4300'
+   }
+  }
+
   return config;
 });
