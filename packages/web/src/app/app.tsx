879f85a2ae98a990611642a5d0ae6a90ced9cdf4
Adrian Rocke <me@adrianrocke.com>
2023-02-25 11:49:09 -0600

    build: set up web package

---
+++ packages/web/src/app/app.tsx
@@ -0,0 +1,17 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Route, Routes, Link } from 'react-router-dom';
+
+export function App() {
+  return (
+    <>
+      <h1>
+        <span> Hello there, </span>
+        Welcome web 👋
+      </h1>
+
+      <div />
+    </>
+  );
+}
+
+export default App;
