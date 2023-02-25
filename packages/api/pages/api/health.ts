30910e9fd159c2a64cb88e3920fd5bc633e46a63
Adrian Rocke <me@adrianrocke.com>
2023-02-25 12:08:29 -0600

    feat: health check route

--- packages/api/pages/api/health.ts
+++ packages/api/pages/api/health.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default function(req: NextApiRequest, res: NextApiResponse) {
-  return res.status(200).json({ status: 'ok' })
+  return res.status(200).send('ok')
 }
