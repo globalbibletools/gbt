badfd713c999c7068955c8bff371e48a9c59666e
Adrian Rocke <me@adrianrocke.com>
2023-03-08 16:41:46 -0600

    build: remove nextjs rewrites

--- packages/api/next.config.js
+++ packages/api/next.config.js
@@ -3,27 +3,10 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { withNx } = require('@nrwl/next/plugins/with-nx');
 
-
 /**
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
-  async rewrites() {
-    let apiURL = process.env['API_URL']
-
-    if (!apiURL) {
-      const vercelURL = process.env['VERCEL_URL']
-      apiURL = `https://${vercelURL.replace('gloss-translation', 'gloss-translation-api')}`
-    }
-    console.log(apiURL)
-
-    return [
-      {
-        "source": "/api/:match*",
-        "destination": `${apiURL}/api/:match*`
-      },
-    ]
-  },
   nx: {
     // Set this to true if you would like to to use SVGR
     // See: https://github.com/gregberge/svgr
