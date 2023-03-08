1a24b015aa329f64ad229882fd0f3d79d31591f3
Adrian Rocke <me@adrianrocke.com>
2023-03-08 16:27:32 -0600

    refactor: nextjs rewrites

--- packages/api/next.config.js
+++ packages/api/next.config.js
@@ -3,19 +3,20 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { withNx } = require('@nrwl/next/plugins/with-nx');
 
-let apiURL = process.env['API_URL']
-
-if (!apiURL) {
-  const vercelURL = process.env['VERCEL_URL']
-  apiURL = `https://${vercelURL.replace('gloss-translation', 'gloss-translation-api')}`
-}
-console.log(apiURL)
 
 /**
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
   async rewrites() {
+    let apiURL = process.env['API_URL']
+
+    if (!apiURL) {
+      const vercelURL = process.env['VERCEL_URL']
+      apiURL = `https://${vercelURL.replace('gloss-translation', 'gloss-translation-api')}`
+    }
+    console.log(apiURL)
+
     return [
       {
         "source": "/api/:match*",
