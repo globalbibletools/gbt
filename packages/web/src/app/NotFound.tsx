e458a32614808a6fb4a66150e19dffe673958816
Adrian Rocke <me@adrianrocke.com>
2023-03-15 09:36:03 -0500

    refactor: error message

--- packages/web/src/app/NotFound.tsx
+++ packages/web/src/app/NotFound.tsx
@@ -6,7 +6,7 @@ export default function NotFound() {
 
   return <div className="absolute w-full h-full flex flex-col items-center justify-center">
     <h1 className="font-bold text-xl mb-6">Woops!</h1>
-    <p className="mb-2">We couldn't find what you are looking for</p>
+    <p className="mb-2">An unknown error has occurred.</p>
     <p className="italic">
       {error.statusText || error.message}
     </p>
