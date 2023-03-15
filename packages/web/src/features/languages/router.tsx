68a98fe4be6dfd0b53f80a2a444b7a2c63b5bd21
Adrian Rocke <me@adrianrocke.com>
2023-03-15 08:20:14 -0500

    chore: app layout

---
+++ packages/web/src/features/languages/router.tsx
@@ -0,0 +1,9 @@
+import { RouteObject } from "react-router-dom"
+import LanguageView from "./LanguageView"
+
+const routes: RouteObject[] = [{
+  path: 'languages',
+  element: <LanguageView />
+}]
+
+export default routes
--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -1,16 +1,14 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-
-import App from './app/app';
+import { RouterProvider } from 'react-router-dom';
+import router from './app/router'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>
 );
