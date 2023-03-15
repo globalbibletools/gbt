bda05bce7c0ec75d17cef10ef427e444d5b233e8
Adrian Rocke <me@adrianrocke.com>
2023-03-14 20:29:12 -0500

    refactor: pr cleanup

--- packages/api/pages/api/health.ts
+++ packages/api/pages/api/health.ts
@@ -2,9 +2,8 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { client } from '../../db'
 
 export default async function(req: NextApiRequest, res: NextApiResponse) {
-  const collections = await client.db().listCollections().toArray()
+  await client.db().listCollections().toArray()
   return res.status(200).json({
-    status: 'ok',
-    collections
+    databaseConnection: 'ok'
   })
 }
