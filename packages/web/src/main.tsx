f58a1cdc52e9f020ffec0384149604436c5de1b0
Adrian Rocke <me@adrianrocke.com>
2023-03-24 16:12:11 -0500

    chore: set up internationalization (#37)

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import router from './app/router'
+import router from './app/router';
+import './app/i18n';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
