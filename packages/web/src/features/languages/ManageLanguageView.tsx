3fab6522ebb571400d683c9d0965cd14abded7a3
Adrian Rocke <me@adrianrocke.com>
2023-04-01 18:04:36 -0500

    feat: update languages

---
+++ packages/web/src/features/languages/ManageLanguageView.tsx
@@ -0,0 +1,63 @@
+import apiClient from '../../shared/apiClient';
+import View from '../../shared/components/View';
+import ViewTitle from '../../shared/components/ViewTitle';
+import { FormEvent } from 'react';
+import {
+  useNavigate,
+  useParams,
+  LoaderFunctionArgs,
+  useLoaderData,
+} from 'react-router-dom';
+import TextInput from '../../shared/components/TextInput';
+import FormLabel from '../../shared/components/FormLabel';
+import Button from '../../shared/components/Button';
+import { useFetch } from '../../shared/useFetch';
+import { GetLanguageResponseBody } from '@translation/api-types';
+
+export async function manageLanguageViewLoader({ params }: LoaderFunctionArgs) {
+  return apiClient.languages.findByCode(params.code ?? 'unknown');
+}
+
+export default function ManageLanguageView() {
+  const language = useLoaderData() as GetLanguageResponseBody;
+
+  async function onSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const name = (
+      e.currentTarget.elements.namedItem('name') as HTMLInputElement
+    ).value;
+    await apiClient.languages.update({
+      data: {
+        type: 'language',
+        id: language.data.id,
+        attributes: {
+          name,
+        },
+      },
+    });
+  }
+
+  return (
+    <View fitToScreen>
+      <div className="m-auto w-fit">
+        <ViewTitle>{language.data.attributes.name}</ViewTitle>
+
+        <form onSubmit={onSubmit}>
+          <div className="mb-4">
+            <FormLabel htmlFor="name">NAME</FormLabel>
+            <TextInput
+              id="name"
+              name="name"
+              className="block"
+              autoComplete="off"
+              defaultValue={language.data.attributes.name}
+            />
+          </div>
+          <div>
+            <Button type="submit">Update</Button>
+          </div>
+        </form>
+      </div>
+    </View>
+  );
+}
--- packages/web/src/features/languages/router.tsx
+++ packages/web/src/features/languages/router.tsx
@@ -1,5 +1,8 @@
 import { RouteObject } from 'react-router-dom';
 import LanguageView from './LanguageView';
+import ManageLanguageView, {
+  manageLanguageViewLoader,
+} from './ManageLanguageView';
 import NewLanguageView from './NewLanguageView';
 
 const routes: RouteObject[] = [
@@ -10,6 +13,11 @@ const routes: RouteObject[] = [
   {
     path: 'languages/new',
     element: <NewLanguageView />,
+  },
+  {
+    path: 'languages/:code',
+    loader: manageLanguageViewLoader,
+    element: <ManageLanguageView />,
   },
 ];
 
