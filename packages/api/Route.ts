b9eb94d1be2c493032642f891a0586f3ff643c9f
Adrian Rocke <me@adrianrocke.com>
2023-04-08 08:25:57 -0500

    docs: document how to build api routes

--- packages/api/Route.ts
+++ packages/api/Route.ts
@@ -4,14 +4,25 @@ import { ZodSchema } from 'zod';
 import { Prisma } from './prisma/client';
 
 export interface ResponseHelper<Body> {
-  ok(data: Body): void;
+  /** Returns 200 or 204 dependening on whether there is a response body to send. */
+  ok(data?: Body): void;
+  /** Returns 201 created with the contents of the new resource. */
   created(data: Body): void;
+  /** Returns 404 with a list of errors. By default it will have a single error with code NotFound. */
   notFound(errors?: Error[]): void;
+  /** Returns 409 with a list of errors. */
   conflict(errors: Error[]): void;
+  /** Returns 422 with a list of errors. */
   invalid(errors: Error[]): void;
 }
 
 export type RouteDefinition<Params, RequestBody, ResponseBody> = {
+  /**
+   * Defines the implementation of the route.
+   * When this is called, `req.body` will have been parsed and should match the `RequestBody` type.
+   * @param req The nextjs request object with typed query and body.
+   * @param res The response helper wrapper around the nextjs response object.
+   */
   handler(
     req: Omit<NextApiRequest, 'query' | 'body'> & {
       query: Params;
@@ -23,24 +34,62 @@ export type RouteDefinition<Params, RequestBody, ResponseBody> = {
   ? // eslint-disable-next-line @typescript-eslint/ban-types
     {}
   : {
+      /**
+       * The `zod` schema used to parse the request body.
+       * This schema should produce an object that matches the `RequestType`.
+       * If `RequestBody` is `void` then a schema is not required.
+       */
       schema:
         | ZodSchema<RequestBody>
         | ((req: NextApiRequest & { query: Params }) => ZodSchema<RequestBody>);
     });
 
 export interface RouteBuilder<Params> {
+  /**
+   * Creates a nextjs API route handler.
+   * This will compile the handlers for each method into a single handler.
+   * It also handles 405 responses for unsupported methods.
+   */
   build(): NextApiHandler;
+  /**
+   * Creates a GET route handler
+   * @template RequestBody The type of the HTTP request body. This should be validated by the `schema`.
+   * @template ResponseBody The type of the HTTP response body. This route should return a value that matches this type.
+   */
   get<RequestBody, ResponseBody>(
     definition: RouteDefinition<Params, RequestBody, ResponseBody>
   ): RouteBuilder<Params>;
+  /**
+   * Creates a PATCH route handler
+   * @template RequestBody The type of the HTTP request body. This should be validated by the `schema`.
+   * @template ResponseBody The type of the HTTP response body. This route should return a value that matches this type.
+   */
   patch<RequestBody, ResponseBody>(
     definition: RouteDefinition<Params, RequestBody, ResponseBody>
   ): RouteBuilder<Params>;
+  /**
+   * Creates a POST route handler
+   * @template RequestBody The type of the HTTP request body. This should be validated by the `schema`.
+   * @template ResponseBody The type of the HTTP response body. This route should return a value that matches this type.
+   */
   post<RequestBody, ResponseBody>(
+    definition: RouteDefinition<Params, RequestBody, ResponseBody>
+  ): RouteBuilder<Params>;
+  /**
+   * Creates a DELETE route handler
+   * @template RequestBody The type of the HTTP request body. This should be validated by the `schema`.
+   * @template ResponseBody The type of the HTTP response body. This route should return a value that matches this type.
+   */
+  delete<RequestBody, ResponseBody>(
     definition: RouteDefinition<Params, RequestBody, ResponseBody>
   ): RouteBuilder<Params>;
 }
 
+/**
+ * Creates a route handler for a nextjs API route.
+ * The result of `build()` is exported and consumed by nextjs.
+ * @template Params The type of the URL parameters as defined in the nextjs file names.
+ */
 export default function createRoute<Params>(): RouteBuilder<Params> {
   const handlers: { [method: string]: NextApiHandler } = {};
 
@@ -145,6 +194,12 @@ export default function createRoute<Params>(): RouteBuilder<Params> {
       definition: RouteDefinition<Params, RequestBody, ResponseBody>
     ) {
       handlers['PATCH'] = createHandler(definition);
+      return this;
+    },
+    delete<RequestBody, ResponseBody>(
+      definition: RouteDefinition<Params, RequestBody, ResponseBody>
+    ) {
+      handlers['DELETE'] = createHandler(definition);
       return this;
     },
     build() {
