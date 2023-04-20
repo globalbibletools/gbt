9b6607dcf74337a6acb4a3fe4ffe7e5e50bca9c9
Adrian Rocke <me@adrianrocke.com>
2023-04-20 17:29:01 -0500

    refactor: update db model for seed data

--- packages/api/prisma/seed.ts
+++ packages/api/prisma/seed.ts
@@ -1,16 +1,15 @@
-import data from '../../../data/morphology'
+import data from '../../../data/morphology';
 import { PrismaClient } from '../prisma/client';
 
 const client = new PrismaClient();
 
 async function run() {
-	// TODO: generate migration to rename search field to name.
-	// TODO: order books in morphology import so IDs are correct.
-	await client.book.createMany({
-		data: Object.keys(data).map((name, id) => ({ id: id + 1, name }))
-	})
+  // TODO: order books in morphology import so IDs are correct.
+  await client.book.createMany({
+    data: Object.keys(data).map((name, id) => ({ id: id + 1, name })),
+  });
 
-	await client.$disconnect()
+  await client.$disconnect();
 }
 
-run()
+run();
