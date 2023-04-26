741dcaea61d30697fb0f0f6d912a149e7600764c
Adrian Rocke <me@adrianrocke.com>
2023-04-26 12:19:59 -0500

    feat: display english glosses under original languages

--- packages/web/src/features/translation/TranslationView.tsx
+++ packages/web/src/features/translation/TranslationView.tsx
@@ -1,14 +1,26 @@
 import { LoaderFunctionArgs, useLoaderData } from 'react-router-dom';
 import apiClient from '../../shared/apiClient';
 import { useLayoutContext } from '../../app/Layout';
-import { GetVerseResponseBody } from '@translation/api-types';
+import {
+  GetVerseGlossesResponseBody,
+  GetVerseResponseBody,
+} from '@translation/api-types';
 
 export async function translationViewLoader({ params }: LoaderFunctionArgs) {
-  return apiClient.verses.findById(params.verseId ?? 'unknown');
+  const verse = await apiClient.verses.findById(params.verseId ?? 'unknown');
+  const referenceGlosses = await apiClient.verses.findVerseGlosses(
+    params.verseId ?? 'unknown',
+    'en'
+  );
+
+  return { verse, referenceGlosses };
 }
 
 export default function TranslationView() {
-  const verse = useLoaderData() as GetVerseResponseBody;
+  const { verse, referenceGlosses } = useLoaderData() as {
+    verse: GetVerseResponseBody;
+    referenceGlosses: GetVerseGlossesResponseBody;
+  };
   const bookId = parseInt(verse.data.id.slice(0, 2));
   const hebrew = bookId < 40;
 
@@ -17,9 +29,12 @@ export default function TranslationView() {
   return (
     <div className="px-4">
       <ol className={`flex flex-wrap ${hebrew ? 'flex-row-reverse' : ''}`}>
-        {verse.data.words.map((word) => (
-          <li key={word.id} className="mx-2">
-            <div className="font-serif text-2xl">{word.text}</div>
+        {verse.data.words.map((word, i) => (
+          <li key={word.id} className="mx-2 mb-4">
+            <div className={`font-serif ${hebrew ? 'text-2xl' : 'text-lg'}`}>
+              {word.text}
+            </div>
+            <div className="">{referenceGlosses.data[i]?.gloss}</div>
           </li>
         ))}
       </ol>
