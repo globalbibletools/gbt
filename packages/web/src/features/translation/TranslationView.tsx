a6d341513b96ce7ea300ea09e8649742e931e209
Adrian Rocke <me@adrianrocke.com>
2023-04-29 17:11:42 -0500

    feat: save new glosses

--- packages/web/src/features/translation/TranslationView.tsx
+++ packages/web/src/features/translation/TranslationView.tsx
@@ -1,8 +1,9 @@
 import { useParams } from 'react-router-dom';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useMutation } from '@tanstack/react-query';
 import apiClient from '../../shared/apiClient';
 import { useLayoutContext } from '../../app/Layout';
 import TextInput from '../../shared/components/TextInput';
+import { useState } from 'react';
 
 export default function TranslationView() {
   const params = useParams() as { verseId: string };
@@ -19,6 +20,11 @@ export default function TranslationView() {
     ['verse-glosses', language, params.verseId],
     () => apiClient.verses.findVerseGlosses(params.verseId, language)
   );
+
+  const glossMutation = useMutation({
+    mutationFn: (variables: { wordId: string; gloss: string }) =>
+      apiClient.words.updateGloss(variables.wordId, language, variables.gloss),
+  });
 
   const loading =
     !verseQuery.isSuccess ||
@@ -39,22 +45,35 @@ export default function TranslationView() {
   return (
     <div className="px-4">
       <ol className={`flex flex-wrap ${hebrew ? 'flex-row-reverse' : ''}`}>
-        {verse.words.map((word, i) => (
-          <li key={word.id} className="mx-2 mb-4 w-36">
-            <div
-              className={`font-serif ${
-                hebrew ? 'text-2xl text-right' : 'text-lg'
-              }`}
-            >
-              {word.text}
-            </div>
-            <div>{referenceGlosses[i]?.gloss}</div>
-            <TextInput
-              className="w-full"
-              defaultValue={targetGlosses[i]?.gloss}
-            />
-          </li>
-        ))}
+        {verse.words.map((word, i) => {
+          const targetGloss = targetGlosses[i]?.gloss;
+
+          return (
+            <li key={word.id} className="mx-2 mb-4 w-36">
+              <div
+                className={`font-serif ${
+                  hebrew ? 'text-2xl text-right' : 'text-lg'
+                }`}
+              >
+                {word.text}
+              </div>
+              <div>{referenceGlosses[i]?.gloss}</div>
+              <TextInput
+                className="w-full"
+                defaultValue={targetGloss}
+                onBlur={async (e) => {
+                  const newGloss = e.currentTarget.value;
+                  if (newGloss !== targetGloss) {
+                    await glossMutation.mutateAsync({
+                      wordId: word.id,
+                      gloss: e.currentTarget.value,
+                    });
+                  }
+                }}
+              />
+            </li>
+          );
+        })}
       </ol>
     </div>
   );
