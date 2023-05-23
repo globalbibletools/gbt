aad9de65ecdde195ab6fb67e8265bf7c192507f1
Pertempto <pertemptodisce@gmail.com>
2023-05-23 19:40:12 -0400

    Position text elements off screen.

--- packages/web/src/shared/hooks/useTextWidth.ts
+++ packages/web/src/shared/hooks/useTextWidth.ts
@@ -5,26 +5,34 @@ import { useEffect, useRef, useState } from 'react';
  * @param text The text to measure.
  * @returns The width of the text, in pixels.
  */
+// TODO: allow the font information to be passed in, so that we can take
+//       into account things like font family, font size, and font weight.
 export function useTextWidth(text: string): number {
-  // TODO: allow the font information to be passed in, so that we can take
-  //       into account things like font family, font size, and font weight.
   let textElement: HTMLElement;
+
   useEffect(() => {
     textElement = document.createElement('span');
     textElement.style.width = 'auto';
     textElement.style.height = '0';
-    textElement.style.position = 'absolute';
+    textElement.style.maxHeight = '0';
     textElement.style.visibility = 'hidden';
+    textElement.style.position = 'absolute';
+    // This seems hacky, but it gets the elements completely off the screen, so
+    // that they don't affect the scroll bar.
+    textElement.style.top = '-9999px';
+    textElement.style.left = '-9999px';
     document.body.appendChild(textElement);
     return () => textElement.remove();
   }, []);
 
   const [width, setWidth] = useState(0);
+
   useEffect(() => {
     if (textElement) {
       textElement.innerText = text;
       setWidth(textElement.clientWidth);
     }
   }, [text]);
+
   return width;
 }
