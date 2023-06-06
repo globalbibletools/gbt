1e67d95dec932183f42439453df6fe3f1928a4d0
Adrian Rocke <me@adrianrocke.com>
2023-06-05 19:07:18 -0500

    build: use repo owner to generate api url

--- packages/web/webpack.config.js
+++ packages/web/webpack.config.js
@@ -12,7 +12,7 @@ module.exports = composePlugins(withNx(), withReact(), (config) => {
           ? `https://gloss-translation-api-git-${process.env.VERCEL_GIT_COMMIT_REF.replace(
               '/',
               '-'
-            )}-${process.env.VERCEL_GIT_COMMIT_AUTHOR_LOGIN}.vercel.app`
+            )}-${process.env.VERCEL_GIT_REPO_OWNER}.vercel.app`
           : process.env.API_URL
       ),
     })
