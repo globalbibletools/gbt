8ab0a21dd3f3748fecabf2b159a997373655b802
Adrian Rocke <me@adrianrocke.com>
2023-06-11 22:04:25 -0500

    build: upgrade nx

--- packages/web/webpack.config.js
+++ packages/web/webpack.config.js
@@ -1,6 +1,6 @@
 const { DefinePlugin } = require('webpack');
-const { composePlugins, withNx } = require('@nrwl/webpack');
-const { withReact } = require('@nrwl/react');
+const { composePlugins, withNx } = require('@nx/webpack');
+const { withReact } = require('@nx/react');
 
 module.exports = composePlugins(withNx(), withReact(), (config) => {
   config.plugins ??= [];
