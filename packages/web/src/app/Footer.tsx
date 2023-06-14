01435abd5edb0ae8a66500ac7491912ae6d6d521
Pertempto <pertemptodisce@gmail.com>
2023-06-14 05:45:40 -0400

    First basic footer.

---
+++ packages/web/src/app/Footer.tsx
@@ -0,0 +1,28 @@
+import { useRef } from 'react';
+import { useTranslation } from 'react-i18next';
+import { DialogRef } from '../shared/components/Dialog';
+import { Icon } from '../shared/components/Icon';
+import LanguageDialog from './LanguageDialog';
+import interfaceLanguages from './languages.json';
+
+export default function Footer() {
+  const languageDialog = useRef<DialogRef>(null);
+  const { t, i18n } = useTranslation();
+
+  return (
+    <div className="p-2 flex flex-row z-10 justify-end">
+      <button
+        type="button"
+        onClick={() => {
+          languageDialog.current?.open();
+        }}
+      >
+        <Icon icon="earth" className="me-2" fixedWidth />
+        {(interfaceLanguages as { [code: string]: string })[
+          i18n.resolvedLanguage
+        ] ?? t('language', { count: 100 })}
+      </button>
+      <LanguageDialog ref={languageDialog} />
+    </div>
+  );
+}
