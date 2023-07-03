6393248de1019258b9d4b19fab6208a4f681ac60
Adrian Rocke <me@adrianrocke.com>
2023-07-02 21:18:00 -0500

    chore: migrate existing users to be verified

--- packages/api/prisma/migrations/20230702224847_add_email_statuses/migration.sql
+++ packages/api/prisma/migrations/20230702224847_add_email_statuses/migration.sql
@@ -1,12 +1,18 @@
 /*
-  Warnings:
-
-  - You are about to drop the column `emailVerified` on the `User` table. All the data in the column will be lost.
-
-*/
+ Warnings:
+ 
+ - You are about to drop the column `emailVerified` on the `User` table. All the data in the column will be lost.
+ 
+ */
 -- CreateEnum
-CREATE TYPE "EmailStatus" AS ENUM ('UNVERIFIED', 'VERIFIED', 'BOUNCED', 'COMPLAINED');
-
+CREATE TYPE "EmailStatus" AS ENUM (
+  'UNVERIFIED',
+  'VERIFIED',
+  'BOUNCED',
+  'COMPLAINED'
+);
 -- AlterTable
 ALTER TABLE "User" DROP COLUMN "emailVerified",
-ADD COLUMN     "emailStatus" "EmailStatus" NOT NULL DEFAULT 'UNVERIFIED';
+  ADD COLUMN "emailStatus" "EmailStatus" NOT NULL DEFAULT 'UNVERIFIED';
+UPDATE "User"
+SET emailStatus = "VERIFIED";
