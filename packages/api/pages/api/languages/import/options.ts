256d3d37341063e9b6617aa566dda69c6677ffb3
Pertempto <pertemptodisce@gmail.com>
2023-07-07 17:34:27 -0400

    Implement basic request to server.

--- packages/api/pages/api/languages/import/options.ts
+++ packages/api/pages/api/languages/import/options.ts
@@ -2,6 +2,8 @@ import { GetLanguageImportOptionsResponseBody } from '@translation/api-types';
 import createRoute from '../../../../shared/Route';
 import { authorize } from '../../../../shared/access-control/authorize';
 
+const IMPORT_SERVER = 'https://hebrewgreekbible.online';
+
 export default createRoute()
   .get<void, GetLanguageImportOptionsResponseBody>({
     authorize: authorize({
@@ -9,9 +11,27 @@ export default createRoute()
       subject: 'AuthUser',
     }),
     async handler(req, res) {
-      res.ok({
-        data: ['test', 'another one'],
-      });
+      try {
+        // Request the server root page.
+        const response = await fetch(IMPORT_SERVER);
+        const html = await response.text();
+        // Parse the HTML for the language options.
+        const regex = /var glossLanguageNames\s*=\s*\[([\s\S]*?)\];/;
+        const matches = html.match(regex);
+        if (matches && matches[1]) {
+          const languageNames = matches[1]
+            .split(',')
+            .map((name: string) => name.trim().replace(/['"]+/g, ''))
+            .filter((name: string) => name.length > 0);
+          // Return the options.
+          res.ok({ data: languageNames });
+        }
+      } catch (error) {
+        console.error('Error retrieving options:', error);
+        // TODO: it would probably be better to throw an actual error?
+      }
+      // There was some problem with retrieving language options.
+      res.ok({ data: [] });
     },
   })
   .build();
