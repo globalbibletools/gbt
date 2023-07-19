4b1f02e644f98b99de12c9906cc776bd8323c1ff
Adrian Rocke <me@adrianrocke.com>
2023-07-19 10:52:38 -0500

    docs: query stale time docs

--- packages/web/src/shared/queryClient.tsx
+++ packages/web/src/shared/queryClient.tsx
@@ -5,6 +5,9 @@ export default queryClient;
 
 queryClient.setDefaultOptions({
   queries: {
+    // This stale time prevents refetches for 10 minutes after data is loaded.
+    // If a query is invalidated, it will still be refetched,
+    // but this prevents continually refetching queries that are used in multiple places like the user session.
     staleTime: 10 * 60 * 1000, // 10 minutes,
   },
 });
