8b9d9fdc948a6e9aea7b667e5f7846d23e0bae3f
Pertempto <pertemptodisce@gmail.com>
2023-07-28 05:55:25 -0400

    Start trying to import actual data. WIP

--- packages/api/pages/api/languages/[code]/import.ts
+++ packages/api/pages/api/languages/[code]/import.ts
@@ -6,6 +6,7 @@ import { z } from 'zod';
 import createRoute from '../../../../shared/Route';
 import { authorize } from '../../../../shared/access-control/authorize';
 import { importServer } from '../../../../shared/env';
+import { client } from '../../../../shared/db';
 
 export default createRoute()
   .post<PostLanguageImportRequestBody, void>({
@@ -18,6 +19,26 @@ export default createRoute()
       import: z.string(),
     }),
     async handler(req, res) {
+      const language = await client.language.findUnique({
+        where: {
+          code: req.query.code,
+        },
+      });
+
+      if (!language) {
+        res.notFound();
+        return;
+      }
+
+      // Delete all the glosses for the language.
+      await client.gloss.deleteMany({
+        where: {
+          languageId: language.id,
+        },
+      });
+
+      const glossData = [];
+
       // TODO: we only use a few book keys for testing, to avoid creating a
       //       ton of requests. use all keys when things are working
       // for (const key of bookKeys) {
@@ -25,11 +46,60 @@ export default createRoute()
         const importUrl = `${importServer}/${req.body.import}Glosses/${key}Gloss.js`;
         const response = await fetch(importUrl);
         const jsCode = await response.text();
-        const glossArray = parseGlossJs(jsCode);
-        console.log(glossArray.length);
-        // TODO: delete all glosses for the book.
-        // TODO: insert all parsed glosses for the book.
+        const bookData = parseGlossJs(jsCode);
+        const bookId = bookKeys.indexOf(key) + 1;
+        // Accumulate gloss data
+        for (
+          let chapterNumber = 1;
+          chapterNumber <= bookData.length;
+          chapterNumber++
+        ) {
+          const chapterData = bookData[chapterNumber - 1];
+          for (
+            let verseNumber = 1;
+            verseNumber <= chapterData.length;
+            verseNumber++
+          ) {
+            const verseData = chapterData[verseNumber - 1];
+            for (
+              let wordNumber = 1;
+              wordNumber <= verseData.length;
+              wordNumber++
+            ) {
+              const wordId = [
+                bookId.toString().padStart(2, '0'),
+                chapterNumber.toString().padStart(3, '0'),
+                verseNumber.toString().padStart(3, '0'),
+                // TODO: the +1 is just to work around a current bug
+                (wordNumber + 1).toString().padStart(2, '0'),
+              ].join('');
+              console.log(wordId);
+              const gloss = verseData[wordNumber - 1][0];
+              glossData.push({ wordId, gloss });
+              console.log(gloss);
+              // TODO: handle situation where the gloss data has extra words.
+              await client.gloss.create({
+                data: {
+                  wordId,
+                  languageId: language.id,
+                  gloss,
+                },
+              });
+            }
+          }
+        }
       }
+
+      // Actually insert all the glosses.
+      // const result = await client.gloss.createMany({
+      //   data: glossData.map(({ wordId, gloss }) => ({
+      //     wordId,
+      //     languageId: language.id,
+      //     gloss,
+      //   })),
+      // });
+      // console.log('Number of glosses imported:', result.count);
+
       res.ok();
     },
   })
