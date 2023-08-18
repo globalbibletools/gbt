a3d6d40793a025205ace2a0cb601871eb73eb20e
Adrian Rocke <me@adrianrocke.com>
2023-08-18 17:21:39 -0500

    refactor: simplify prisma setup

--- packages/api/next.config.js
+++ packages/api/next.config.js
@@ -1,7 +1,6 @@
 //@ts-check
 
 const { withNx } = require('@nx/next/plugins/with-nx');
-const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin');
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
@@ -11,13 +10,6 @@ const nextConfig = {
     // Set this to true if you would like to to use SVGR
     // See: https://github.com/gregberge/svgr
     svgr: false,
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.plugins = [...config.plugins, new PrismaPlugin()];
-    }
-
-    return config;
   },
   experimental: {
     outputFileTracingExcludes: {
