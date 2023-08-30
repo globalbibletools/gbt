86a152e9a9324316e2bf74508f1256fc938da001
Addison Emig <pertemptodisce@gmail.com>
2023-08-29 21:59:57 -0400

    refactor: use db package from lambda function (#151)
    Co-authored-by: Adrian Rocke <me@adrianrocke.com>

--- packages/api/shared/env.ts
+++ packages/api/shared/env.ts
@@ -20,3 +20,4 @@ export const redirectOrigin =
     : process.env.REDIRECT_ORIGIN;
 
 export const importServer = 'https://hebrewgreekbible.online';
+export const importTriggerUrl = process.env.IMPORT_URL ?? '';
