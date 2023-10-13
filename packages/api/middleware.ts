71c9faccdb5a07b65e1751811da720a0da0682ab
Adrian Rocke <me@adrianrocke.com>
2023-10-12 20:35:41 -0500

    chore: log URLs

---
+++ packages/api/middleware.ts
@@ -0,0 +1,8 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  console.log(request.url);
+  return NextResponse.next();
+}
