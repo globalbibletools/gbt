38f52ea8c3d2778bb899d93123be1ca95fbe3218
Adrian Rocke <me@adrianrocke.com>
2023-10-27 21:47:01 -0500

    fix: remove console.log

--- packages/api/shared/machine-translation.tsx
+++ packages/api/shared/machine-translation.tsx
@@ -3,9 +3,6 @@ import { TranslationServiceClient } from '@google-cloud/translate';
 export class MachineTranslationClient {
   client: TranslationServiceClient;
   constructor() {
-    console.log(
-      process.env.GOOGLE_TRANSLATE_CREDENTIALS?.replaceAll('\n', '\\n') ?? ''
-    );
     this.client = new TranslationServiceClient({
       credentials: JSON.parse(
         process.env.GOOGLE_TRANSLATE_CREDENTIALS?.replaceAll('\n', '\\n') ?? ''
