4d907dcfc80d3314047ffec1d4bccf798664e4a2
Pertempto <pertemptodisce@gmail.com>
2023-11-09 04:57:34 -0500

    Use hebrew and greek dictionary JS files

---
+++ data/strongs.ts
@@ -0,0 +1,8 @@
+export const hebrewStrongsData: Record<
+  string,
+  Record<string, string>
+> = require(`./strongs/strongs-hebrew-dictionary.js`);
+export const greekStrongsData: Record<
+  string,
+  Record<string, string>
+> = require(`./strongs/strongs-greek-dictionary.js`);
