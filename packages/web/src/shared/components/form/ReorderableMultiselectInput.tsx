5c520f661fc34e748ea2c906e7858f8c75749fc8
Pertempto <pertemptodisce@gmail.com>
2023-12-04 04:57:38 -0500

    Try to use combobox for new item selector.

--- packages/web/src/shared/components/form/ReorderableMultiselectInput.tsx
+++ packages/web/src/shared/components/form/ReorderableMultiselectInput.tsx
@@ -1,9 +1,10 @@
-import { Combobox } from '@headlessui/react';
 import { forwardRef } from 'react';
-import { Controller, useFormContext } from 'react-hook-form';
+import { Controller, useForm, useFormContext } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import { Icon } from '../Icon';
 import Button from '../actions/Button';
+import ComboboxInput from './ComboboxInput';
+import Form from './Form';
 
 export type ReorderableMultiselectInputProps =
   BaseReorderableMultiselectInputProps & {
@@ -51,6 +52,8 @@ interface BaseReorderableMultiselectInputProps {
 
 type ItemType = { label: string; value: string };
 
+type NewItemFormData = { newItem: string };
+
 const BaseReorderableMultiselectInput = forwardRef<
   HTMLInputElement,
   BaseReorderableMultiselectInputProps
@@ -70,6 +73,14 @@ const BaseReorderableMultiselectInput = forwardRef<
     ref
   ) => {
     const { t } = useTranslation(['common']);
+    const newItemContext = useForm<NewItemFormData>();
+    const addItem = (data: NewItemFormData) => {
+      console.log(data);
+      // if (value) {
+      //   const newValue = [...(value ?? []), itemValue];
+      //   onChange?.(newValue);
+      // }
+    };
     const moveItem = (from: number, to: number) => {
       const newValue = [...(value ?? [])];
       newValue.splice(to, 0, newValue.splice(from, 1)[0]);
@@ -135,65 +146,17 @@ const BaseReorderableMultiselectInput = forwardRef<
               );
             })}
         </div>
-        <div className="flex gap-1">
-          <Combobox
-            value={value}
-            onChange={onChange}
-            multiple
-            name={name}
-            defaultValue={defaultValue}
-          >
-            <div
-              className={`
-            border rounded shadow-inner flex grow
-            group-focus-within/multiselect:outline group-focus-within/multiselect:outline-2
-            ${
-              hasErrors
-                ? 'border-red-700 shadow-red-100 group-focus-within/multiselect:outline-red-700'
-                : 'border-slate-400 group-focus-within/multiselect:outline-blue-600'
-            }
-          `}
-            >
-              <Combobox.Input
-                className="w-full py-2 px-3 h-10 rounded-b flex-grow focus:outline-none bg-transparent rounded"
-                readOnly
-                ref={ref}
-                onBlur={onBlur}
-                displayValue={(value: string[]) =>
-                  value
-                    .map((v) => items.find((i) => i.value === v)?.label ?? '')
-                    .join(', ')
-                }
-                placeholder={placeholder}
-              />
-              <Combobox.Button className="w-8">
-                {({ open }) => <Icon icon={open ? 'caret-up' : 'caret-down'} />}
-              </Combobox.Button>
-            </div>
-            <Combobox.Options className="absolute z-20 mt-1 max-h-80 w-full overflow-auto rounded border border-slate-400 bg-white shadow">
-              {items.map((item) => (
-                <Combobox.Option
-                  className="px-3 py-2 ui-active:bg-blue-400"
-                  key={item.value}
-                  value={item.value}
-                >
-                  {({ selected }) => (
-                    <>
-                      <span className="inline-block w-6">
-                        {selected && <Icon icon="check" />}
-                      </span>
-                      {item.label}
-                    </>
-                  )}
-                </Combobox.Option>
-              ))}
-            </Combobox.Options>
-          </Combobox>
-          <Button>
+        <Form
+          className="flex gap-1"
+          context={newItemContext}
+          onSubmit={addItem}
+        >
+          <ComboboxInput name="new" className="grow block" items={items} />
+          <Button type="submit">
             <Icon icon="add" />
             <span className="ms-1">{t('common:add')}</span>
           </Button>
-        </div>
+        </Form>
       </div>
     );
   }
