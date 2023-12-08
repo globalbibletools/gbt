1e06409a791fe5085583dfd898f6c4588fa1e39d
Pertempto <pertemptodisce@gmail.com>
2023-12-08 04:45:51 -0500

    cleanly handle empty state

--- packages/web/src/shared/components/form/ReorderableMultiselectInput.tsx
+++ packages/web/src/shared/components/form/ReorderableMultiselectInput.tsx
@@ -56,76 +56,81 @@ const BaseReorderableMultiselectInput = forwardRef<
   HTMLInputElement,
   BaseReorderableMultiselectInputProps
 >(({ className = '', value, onChange, items }, ref) => {
+  const selected: string[] = value ?? [];
   const { t } = useTranslation(['common']);
   const [newItemValue, setNewItemValue] = useState('');
   const addItem = () => {
     if (newItemValue) {
-      const newValue = [...(value ?? []), newItemValue];
+      const newValue = [...selected, newItemValue];
       onChange?.(newValue);
       setNewItemValue('');
     }
   };
   const moveItem = (from: number, to: number) => {
-    const newValue = [...(value ?? [])];
+    const newValue = [...selected];
     newValue.splice(to, 0, newValue.splice(from, 1)[0]);
     onChange?.(newValue);
   };
   const removeItem = (index: number) => {
-    const newValue = [...(value ?? [])];
+    const newValue = [...selected];
     newValue.splice(index, 1);
     onChange?.(newValue);
   };
   const availableNewItems = items.filter(
-    (item) => !(value ?? []).includes(item.value)
+    (item) => !selected.includes(item.value)
   );
   return (
     <div
       className={`${className} group/multiselect relative flex flex-col gap-1`}
     >
-      <div className="border rounded flex-col shadow-inner flex border-slate-400">
-        {(value ?? [])
-          .map((v) => items.find((i) => i.value === v))
-          .filter((item?: ItemType): item is ItemType => !!item)
-          .map((item, i, value) => {
-            const isFirst = i === 0;
-            const isLast = i === value.length - 1;
-            return (
-              <div className="py-2 px-1 flex items-center" key={item.value}>
-                <span className="grow mx-1">{item.label}</span>
-                <button
-                  className={`w-8 h-8 pt-[2px] ${
-                    isFirst && 'disabled:opacity-25'
-                  }`}
-                  type="button"
-                  disabled={isFirst}
-                  onClick={() => moveItem(i, i - 1)}
-                >
-                  <Icon icon="chevron-up" />
-                  <span className="sr-only">{t('common:direction.up')}</span>
-                </button>
-                <button
-                  className={`w-8 h-8 pb-[2px] ${
-                    isLast && 'disabled:opacity-25'
-                  }`}
-                  type="button"
-                  disabled={isLast}
-                  onClick={() => moveItem(i, i + 1)}
-                >
-                  <Icon icon="chevron-down" />
-                  <span className="sr-only">{t('common:direction.down')}</span>
-                </button>
-                <button
-                  className="w-8 h-8"
-                  type="button"
-                  onClick={() => removeItem(i)}
-                >
-                  <Icon icon="close" />
-                  <span className="sr-only">{t('common:close')}</span>
-                </button>
-              </div>
-            );
-          })}
-      </div>
+      {selected.length > 0 && (
+        <div className="border rounded flex-col shadow-inner flex border-slate-400">
+          {selected
+            .map((v) => items.find((i) => i.value === v))
+            .filter((item?: ItemType): item is ItemType => !!item)
+            .map((item, i, value) => {
+              const isFirst = i === 0;
+              const isLast = i === value.length - 1;
+              return (
+                <div className="py-2 px-1 flex items-center" key={item.value}>
+                  <span className="grow mx-1">{item.label}</span>
+                  <button
+                    className={`w-8 h-8 pt-[2px] ${
+                      isFirst && 'disabled:opacity-25'
+                    }`}
+                    type="button"
+                    disabled={isFirst}
+                    onClick={() => moveItem(i, i - 1)}
+                  >
+                    <Icon icon="chevron-up" />
+                    <span className="sr-only">{t('common:direction.up')}</span>
+                  </button>
+                  <button
+                    className={`w-8 h-8 pb-[2px] ${
+                      isLast && 'disabled:opacity-25'
+                    }`}
+                    type="button"
+                    disabled={isLast}
+                    onClick={() => moveItem(i, i + 1)}
+                  >
+                    <Icon icon="chevron-down" />
+                    <span className="sr-only">
+                      {t('common:direction.down')}
+                    </span>
+                  </button>
+                  <button
+                    className="w-8 h-8"
+                    type="button"
+                    onClick={() => removeItem(i)}
+                  >
+                    <Icon icon="close" />
+                    <span className="sr-only">{t('common:close')}</span>
+                  </button>
+                </div>
+              );
+            })}
+        </div>
+      )}
       <div className="flex gap-1">
         <ComboboxInput
           name="new"
