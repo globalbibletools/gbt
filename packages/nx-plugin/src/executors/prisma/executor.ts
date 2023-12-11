ba43d864d3788eef9fd2cc7b86fd97dd20f6aa52
ilearner777 <ilearner@protonmail.com>
2023-12-11 14:57:46 -0500

    updated executor.ts with exec command

--- packages/nx-plugin/src/executors/prisma/executor.ts
+++ packages/nx-plugin/src/executors/prisma/executor.ts
@@ -1,5 +1,5 @@
 import { PrismaExecutorSchema } from './schema';
-import { execSync } from 'child_process';
+import { execa } from 'execa';
 
 export default async function runExecutor({
   _,
@@ -9,7 +9,7 @@ export default async function runExecutor({
   // The options are for executors are parsed with yargs, so arguments without a `-` or `--` are added to the `_` property.
   // We assume these are all prisma commands like `prisma migrate reset`.
   // Then we add the schema argument and any remaining standard arguments.
-  const command = `npx prisma ${_.join(
+  const mycommand = `npx prisma ${_.join(
     ' '
   )} --schema=${schema} ${Object.entries(options)
     .map(([key, value]) => `--${key}=${value}`)
@@ -17,8 +17,16 @@ export default async function runExecutor({
 
   // `inherit` allows the parent terminal to send and receive input and output on stdin/out/err.
   // This is necessary because prisma commands are interactive.
-  execSync(command, {
-    stdio: 'inherit',
+  // execSync(command, {
+  //   stdio: 'inherit',
+  // });
+
+  console.info(`Executing mycommand: ${mycommand}`);
+  console.info(`Current working directory: ${process.cwd()}`);
+
+  await execa.command(mycommand, {
+    cwd: process.cwd(),
+    stdio: [process.stdin, process.stdout, 'pipe'],
   });
 
   return {
