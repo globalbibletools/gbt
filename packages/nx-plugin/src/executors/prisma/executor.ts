63d730cee46aa5b7ad7519fd2179fc1d06a5320e
Claudio Bruno <141659916+ilearner777@users.noreply.github.com>
2024-01-03 16:39:00 -0500

    chore: handle interactivity in nx with prisma commands (#299)

--- packages/nx-plugin/src/executors/prisma/executor.ts
+++ packages/nx-plugin/src/executors/prisma/executor.ts
@@ -1,5 +1,5 @@
 import { PrismaExecutorSchema } from './schema';
-import { execSync } from 'child_process';
+import { spawn } from 'child_process';
 
 export default async function runExecutor({
   _,
@@ -9,16 +9,34 @@ export default async function runExecutor({
   // The options are for executors are parsed with yargs, so arguments without a `-` or `--` are added to the `_` property.
   // We assume these are all prisma commands like `prisma migrate reset`.
   // Then we add the schema argument and any remaining standard arguments.
-  const command = `npx prisma ${_.join(
-    ' '
-  )} --schema=${schema} ${Object.entries(options)
-    .map(([key, value]) => `--${key}=${value}`)
-    .join(' ')}`;
 
   // `inherit` allows the parent terminal to send and receive input and output on stdin/out/err.
   // This is necessary because prisma commands are interactive.
-  execSync(command, {
-    stdio: 'inherit',
+  const child = spawn(
+    'npx',
+    [
+      'prisma',
+      ..._,
+      `--schema=${schema}`,
+      ...Object.entries(options).map(([key, value]) => `--${key}=${value}`),
+    ],
+    {
+      stdio: ['inherit', 'pipe', 'inherit'],
+      shell: true,
+    }
+  );
+
+  // But we manually console.log stdout so that it flushes when waiting for stdin.
+  child.stdout.on('data', (chunk) => console.log(chunk.toString()));
+
+  await new Promise<void>((resolve, reject) => {
+    child.on('exit', (code) => {
+      if (code === 0) {
+        resolve();
+      } else {
+        reject();
+      }
+    });
   });
 
   return {
