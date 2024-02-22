0a0a86ac181e5a2910af322779f46a6b4e10c31f
tycebrown <91034018+tycebrown@users.noreply.github.com>
2024-02-21 19:23:51 -0600

    remigrate

---
+++ packages/db/src/migrations/20240222011925_gloss_history_entry/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "GlossHistoryEntry" (
+    "wordId" TEXT NOT NULL,
+    "languageId" UUID NOT NULL,
+    "timestamp" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" UUID,
+    "gloss" TEXT,
+    "state" "GlossState",
+
+    CONSTRAINT "GlossHistoryEntry_pkey" PRIMARY KEY ("wordId","languageId","timestamp")
+);
+
+-- AddForeignKey
+ALTER TABLE "GlossHistoryEntry" ADD CONSTRAINT "GlossHistoryEntry_wordId_fkey" FOREIGN KEY ("wordId") REFERENCES "Word"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "GlossHistoryEntry" ADD CONSTRAINT "GlossHistoryEntry_languageId_fkey" FOREIGN KEY ("languageId") REFERENCES "Language"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "GlossHistoryEntry" ADD CONSTRAINT "GlossHistoryEntry_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
