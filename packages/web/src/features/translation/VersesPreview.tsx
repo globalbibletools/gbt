f388f71ee617b5cd2c7f0ca94cd9e6d159e57bdd
Pertempto <pertemptodisce@gmail.com>
2024-03-25 04:51:23 -0400

    Handle not found situation

--- packages/web/src/features/translation/VersesPreview.tsx
+++ packages/web/src/features/translation/VersesPreview.tsx
@@ -1,17 +1,29 @@
 import { useTranslation } from 'react-i18next';
 import { generateReference, parseVerseId } from './verse-utils';
 import { Icon } from '../../shared/components/Icon';
+import { useState, useEffect } from 'react';
 
 type VersesPreviewProps = { verseIds: string[]; onClose: () => void };
 
 export const VersesPreview = ({ verseIds, onClose }: VersesPreviewProps) => {
   const { t } = useTranslation(['common', 'bible']);
-  const title =
-    generateReference(parseVerseId(verseIds[0]), t) +
-    (verseIds.length > 1
-      ? ' - ' +
-        generateReference(parseVerseId(verseIds[verseIds.length - 1]), t)
-      : '');
+  const [title, setTitle] = useState('');
+  const [isValid, setIsValid] = useState(false);
+  useEffect(() => {
+    try {
+      setTitle(
+        generateReference(parseVerseId(verseIds[0]), t) +
+          (verseIds.length > 1
+            ? ' - ' +
+              generateReference(parseVerseId(verseIds[verseIds.length - 1]), t)
+            : '')
+      );
+      setIsValid(true);
+    } catch {
+      setTitle(t('common:not_found') ?? '');
+      setIsValid(false);
+    }
+  });
   return (
     <div>
       <div className="flex ltr:flex-row rtl:flex-row-reverse items-center justify-between">
